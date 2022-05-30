50782ac15546a62725072ce2ecc50bb2d58299b3
Emmanuel Zelarayan <emmavz@live.com>
2022-05-30 15:18:32 -0300

    Sistema de puntajes por puntos y tiempo

--- backend/ListarPuntajes.php
+++ backend/ListarPuntajes.php
@@ -1,13 +1,45 @@
 <?php
+
 use Zelarayan\Usuario;
 
 require_once("../Usuario.php");
 
 $usuarios = Usuario::TraerUsuariosJSON("./archivos/puntajes.json");
-if($usuarios){
+if ($usuarios) {
     // ordeno el array segun sus puntajes
-    usort($usuarios, function($a, $b) {
-        return  $b->puntajes[0] - $a->puntajes[0];
+    usort($usuarios, function ($a, $b) {
+        $minA = intval(explode(":", $a->tiempos[0])[0]);
+        $minB = intval(explode(":", $b->tiempos[0])[0]);
+        $segA = intval(explode(".", explode(":", $a->tiempos[0])[1])[0]);
+        $segB = intval(explode(".", explode(":", $b->tiempos[0])[1])[0]);
+        $milA = intval(explode(".", explode(":", $a->tiempos[0])[1])[1]);
+        $milB = intval(explode(".", explode(":", $b->tiempos[0])[1])[1]);
+
+        $retorno = 0;
+        if (
+            $a->puntajes[0] < $b->puntajes[0] ||
+            ($a->puntajes[0] == $b->puntajes[0] && compararTiempos($minA, $minB, $segA, $segB, $milA, $milB))
+        ) {
+            $retorno = 1;
+        } else {
+            $retorno = -1;
+        }
+        return $retorno;
+        // return  $b->puntajes[0] - $a->puntajes[0];
     });
     echo json_encode($usuarios);
+}
+
+function compararTiempos(int $minA, int $minB, int $segA, int $segB, int $milA, int $milB)
+{
+    $retorno = false;
+    
+    $segundosA = $minA*60 + $segA + $milA/100;
+    $segundosB = $minB*60 + $segB + $milB/100;
+
+    if($segundosA > $segundosB) {
+        $retorno = true;
+    }
+
+    return $retorno;
 }
